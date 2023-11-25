b1fdba426343c0a7f818153a71aa7e2f14948681
William-Fonseca-Geralde <william.geralde@fatec.sp.gov.br>
2023-11-25 11:06:40 -0300

    inicio

---
+++ projeto.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<locale>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+  cout << ""
+
+
+
+
+
+
+
+
+cout << "||||||||||||||||||| " << endl;
+cout << "|"<<" Sabores de Pizza"<<          "|" << endl;
+cout << "|"<<" Queijo"<<          "          |" << endl;
+cout << "|"<<" Frango"<<          "          |" << endl;
+cout << "|"<<" Pizza"<<          "           |" << endl;
+cout << "|"<<" Vento"<<          "           |" << endl;
+cout << "||||||||||||||||||| " << endl;
+  return 0;
+}
